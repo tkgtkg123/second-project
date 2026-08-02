ed4f519b5d4036ca7bb68a9c81c7c4b5515ae2ee
takumi1224 <kamogawa1224@gmail.com>
2026-08-02 22:27:05 +0900

    feat&style:すっぽぬけワードウルフのメニュー画面のクリアボタン作成。デザイン調整

---
+++ pages/すっぽぬけワードウルフ/javascript/menu.js
@@ -0,0 +1,33 @@
+'use strict';
+const wolf_pulldown = document.querySelector(".wolf_pulldown");
+const time_pulldown = document.querySelector(".time_pulldown");
+const item_themas = document.querySelectorAll(".item_thema");
+const item_rules = document.querySelectorAll(".item_rule");
+const thema_clear_btn = document.querySelector("thema_clear");
+const rule_clear_btn = document.querySelector("rule_clear");
+
+for(let i =1; i<=5; i++){
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent =i;
+    wolf_pulldown.appendChild(option);
+};
+
+for(let i =1; i<=10; i++){
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent =i;
+    time_pulldown.appendChild(option);
+};
+
+item_themas.forEach(thema => {
+    thema.addEventListener("click", () =>{
+        thema.classList.toggle("selected_thema");
+    });
+});
+
+item_rules.forEach(rule => {
+    rule.addEventListener("click", () =>{
+        rule.classList.toggle("selected_rule");
+    });
+});
